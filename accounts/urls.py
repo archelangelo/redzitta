b068e8479f45f03819179545c583b13fc16ea519
Yiran Hu <medycc@gmail.com>
2019-12-14 19:36:04 -0500

    Sign up and sign in done.

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LoginView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
