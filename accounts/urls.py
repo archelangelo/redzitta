68bdea7b5e1ca432549e86d62a661086ae1d3720
Yiran Hu <medycc@gmail.com>
2019-12-14 20:33:01 -0500

    Fixed log out bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,6 +19,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LoginView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
